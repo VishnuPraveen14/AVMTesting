00233e526003571f12c1eedb89342054b4a0b276
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-11 20:47:27 +0530

    test

--- backend.tf
+++ backend.tf
@@ -11,3 +11,16 @@
 #   name     = "state-demo"
 #   location = "eastus"
 # }
+terraform {
+backend "azurerm" {
+    resource_group_name   = "vishnuDemo"
+    storage_account_name  = "vishnudemo9934"
+    container_name        = "test"
+    key                   = "terraform.tfstate"
+}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-
-## Section to provide a random Azure region for the resource group
-# This allows us to randomize the region for the resource group.
-# module "regions" {
-#   source  = "Azure/regions/azurerm"
-#   version = ">= 0.3.0"
-# }
-
-# This allows us to randomize the region for the resource group.
-# resource "random_integer" "region_index" {
-#   max = length(module.regions.regions) - 1
-#   min = 0
-# }
-## End of section to provide a random Azure region for the resource group
-
-# # This ensures we have unique CAF compliant names for our resources.
-# module "naming" {
-#   source  = "Azure/naming/azurerm"
-#   version = ">= 0.3.0"
-# }
-
 # This is required for resource modules
 resource "azurerm_resource_group" "this" {
   location = var.location

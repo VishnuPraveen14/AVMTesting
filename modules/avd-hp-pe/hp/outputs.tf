a28a629c7e880b54230c3479cf0f5fcd13b0ab9c
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-12 01:23:01 +0530

    test

--- modules/avd-hp-pe/hp/outputs.tf
+++ modules/avd-hp-pe/hp/outputs.tf
@@ -16,5 +16,5 @@ output "resource" {
 
 output "resource_id" {
   description = "This output is the full output for the resource to allow flexibility to reference all possible values for the resource. Example usage: module.<modulename>.resource.id"
-  value       = azurerm_virtual_desktop_host_pool.this
+  value       = azurerm_virtual_desktop_host_pool.this.id
 }

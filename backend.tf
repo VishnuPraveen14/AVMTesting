d3ff6b987732d9506600fb7601c83a88980d4abd
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-11 16:53:06 +0530

    test

--- backend.tf
+++ backend.tf
@@ -1,8 +1,23 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name   = "vishnuDemo"
     storage_account_name  = "vishnudemo9934"
     container_name        = "test"
     key                   = "terraform.tfstate"
   }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
 }

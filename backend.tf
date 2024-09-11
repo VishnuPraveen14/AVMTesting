b51fc081490a014c1f466c129fd1f849516d6619
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-11 16:35:32 +0530

    test

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "remote" {
-    storage_account_name = "vishnudemo9934"
-    container_name      = "test"
-    key                 = "terraform.tfstate"
-    subscription_id      = "ce326908-2fd8-4d82-aa1f-106533ca8b6a" 
+  backend "azurerm" {
+    resource_group_name   = "vishnuDemo"
+    storage_account_name  = "vishnudemo9934"
+    container_name        = "test"
+    key                   = "terraform.tfstate"
   }
 }

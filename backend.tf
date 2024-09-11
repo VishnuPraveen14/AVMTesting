d5f44d296df599f25303aaa0e9fdcbe3b64ac435
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2024-09-11 16:22:55 +0530

    test

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+    backend "remote" {
+    storage_account_name = "your-storage-account-name"
+    container_name      = "your-container-name"
+    key                 = "terraform.tfstate"
+  }
+}

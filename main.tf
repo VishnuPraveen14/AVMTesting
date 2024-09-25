d665843df444adb18826452bb9b38a61d4ae7a94
VishnuPraveen14 <141312065+VishnuPraveen14@users.noreply.github.com>
2024-09-25 16:43:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,33 +27,40 @@ module "hpavd" {
 }
 
 
-module "avm-res-desktopvirtualization-applicationgroup" {
-  source  = "Azure/avm-res-desktopvirtualization-applicationgroup/azurerm"
-  version = "0.1.5"
-  virtual_desktop_application_group_host_pool_id = module.hpavd.hostpool_id
-  virtual_desktop_application_group_location = var.location
-  virtual_desktop_application_group_name = "vdag-avd-01"
-  virtual_desktop_application_group_type = "Desktop"
-  virtual_desktop_application_group_resource_group_name = azurerm_resource_group.this.name
-}
-
-module "avm-res-desktopvirtualization-applicationgroupremote" {
-  source  = "Azure/avm-res-desktopvirtualization-applicationgroup/azurerm"
-  version = "0.1.5"
-  virtual_desktop_application_group_host_pool_id = module.hpavd.hostpool_id
-  virtual_desktop_application_group_location = var.location
-  virtual_desktop_application_group_name = "vdag-avd-02"
-  virtual_desktop_application_group_type = "RemoteApp"
-  virtual_desktop_application_group_resource_group_name = azurerm_resource_group.this.name
-}
-
-module "avm-res-desktopvirtualization-workspace" {
-  source  = "Azure/avm-res-desktopvirtualization-workspace/azurerm"
-  version = "0.1.5"
-  # insert the 4 required variables here
-  virtual_desktop_workspace_location = var.location
-  resource_group_name = azurerm_resource_group.this.name
-  virtual_desktop_workspace_name = "vdws-avd-01"
-  virtual_desktop_workspace_resource_group_name = azurerm_resource_group.this.name
-
+module "avm-res-storage-storageaccount" {
+  source                                  = "Azure/avm-res-storage-storageaccount/azurerm"
+  version                                 = "0.2.7"
+  name                                    = "satestlumenmsftv"
+  resource_group_name                     = azurerm_resource_group.this.name
+  location                                = var.location
+  # account_tier                            = var.account_tier
+  # account_replication_type                = var.account_replication_type
+  # account_kind                            = var.account_kind
+  # access_tier                             = var.access_tier
+  # tags                                    = local.tags
+  # public_network_access_enabled           = var.public_network_access_enabled
+  # allow_nested_items_to_be_public         = var.sa_allow_nested_items_to_be_public
+  # infrastructure_encryption_enabled       = var.sa_infrastructure_encryption_enabled
+  shared_access_key_enabled               = true
+  # enable_telemetry                        = var.enable_telemetry
+  shares                                  = {
+  share0 = {
+    name        = "fileshare-1"
+    quota       = 10
+    access_tier = "Hot"
+    metadata = {
+      key1 = "value1"
+      key2 = "value2"
+    }
+    signed_identifiers = [
+      {
+        id = "1"
+        access_policy = {
+          expiry_time = "2025-01-01T00:00:00Z"
+          permission  = "r"
+          start_time  = "2024-01-01T00:00:00Z"
+        }
+      }
+    ]
+  }
 }

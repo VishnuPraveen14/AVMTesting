e2659fc1e92325f20462d31fc61fd8f471e386e9
VishnuPraveen14 <141312065+VishnuPraveen14@users.noreply.github.com>
2024-10-17 22:29:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,59 +9,17 @@ module "naming" {
   version = ">= 0.3.0"
 }
 
-# module "avm-res-keyvault-vault_example_default" {
-#   source  = "Azure/avm-res-keyvault-vault/azurerm"
-#   version = "0.9.1"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.this.name
-#   name                = var.keyvault_name
-#   enable_telemetry    = var.enable_telemetry
-#   tenant_id           = var.tenant_id
-# }
-
-module "hpavd" {
-  source = "./modules/avd-hp-pe"
-  enable_telemetry = var.enable_telemetry
-  location = var.location
-  resource_group_name = var.resource_group_name
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 }
 
-
-module "avm-res-storage-storageaccount" {
-  source                                  = "Azure/avm-res-storage-storageaccount/azurerm"
-  version                                 = "0.2.7"
-  name                                    = "satestlumenmsftv"
-  resource_group_name                     = azurerm_resource_group.this.name
-  location                                = var.location
-  # account_tier                            = var.account_tier
-  # account_replication_type                = var.account_replication_type
-  # account_kind                            = var.account_kind
-  # access_tier                             = var.access_tier
-  # tags                                    = local.tags
-  # public_network_access_enabled           = var.public_network_access_enabled
-  # allow_nested_items_to_be_public         = var.sa_allow_nested_items_to_be_public
-  # infrastructure_encryption_enabled       = var.sa_infrastructure_encryption_enabled
-  shared_access_key_enabled               = true
-  # enable_telemetry                        = var.enable_telemetry
-  shares                                  = {
-  share0 = {
-    name        = "fileshare-1"
-    quota       = 10
-    access_tier = "Hot"
-    metadata = {
-      key1 = "value1"
-      key2 = "value2"
-    }
-    signed_identifiers = [
-      {
-        id = "1"
-        access_policy = {
-          expiry_time = "2025-01-01T00:00:00Z"
-          permission  = "r"
-          start_time  = "2024-01-01T00:00:00Z"
-        }
-      }
-    ]
-  }
-}
+# Step 4: Create a subnet within the virtual network
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
